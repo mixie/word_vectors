d121b3ac44de4cfc6c1ae9051d59353191f77bb7
mixie <lyra742@gmail.com>
2017-05-04 17:27:30 +0200

    save

--- source/vectors_compare.py
+++ source/vectors_compare.py
@@ -14,31 +14,32 @@ from keras.layers import Input, Dense, embeddings, merge, Lambda, Flatten,Reshap
 from scipy.stats import spearmanr
 import argparse
 
+#constants
 VECTOR_SIZE = 100
 MAX_NGRAM_NUM = 74
 
+#read command line args
 import_ngrams = None
 simple_model = None
 
 simple_konv_model = None
 
 parser = argparse.ArgumentParser(description='Compare our results with datasets.')
-parser.add_argument('first', nargs=1, help='1. weight file number')
-parser.add_argument('last', nargs=1, help='last weight file number')
+parser.add_argument('weights', nargs=1, help='weight file number')
 parser.add_argument('type', nargs=1, help='type of model')
 parser.add_argument('--imp', nargs='?', help='important ngrams file, if applicable')
 args = parser.parse_args()
 
 nn_type = args.type[0]
-start = int(args.first[0])
-num_input_files = int(args.last[0])
+weights = int(args.weights[0])
 
 if args.imp is not None:
     import_ngrams = helpers.read_ngrams_from_file(args.imp)
 
+# dataset files
+datasets = {"rw":"../testdata/rw.txt","sim":"../testdata/combined.tab"}
 
-datasets = {"rw":"../../../testdata/rw.txt","sim":"../../../testdata/combined.tab"}
-
+# read model, ngrams, words
 with open('model.json', 'r') as json_file:
     model_json = json_file.read()
     model = model_from_json(model_json,custom_objects={"ZeroMaskedEntries": helpers.models.ZeroMaskedEntries})
@@ -61,6 +62,7 @@ with open('all_unique_words.txt', 'r') as input1:
         word_keys[line.strip()]=i
         i += 1
 
+# for model with selected ngrams
 def create_simple_conv_model(my_model):
     model = Sequential()
     conv = convolutional.Convolution1D(VECTOR_SIZE, MAX_NGRAM_NUM, border_mode='valid',input_shape=(MAX_NGRAM_NUM,VECTOR_SIZE))
@@ -71,6 +73,7 @@ def create_simple_conv_model(my_model):
     conv.set_weights(my_model.keras_model.get_layer(my_model.vector_layer_name).get_weights())
     return model
 
+# prediction with zeroed ngrams
 def predict_with_zeroed_ngrams(my_model,word,ngram_keys,keys2,new_model):
     ngram_vector = my_model.predict_ngram_vectors_for_word(word,ngram_keys)
     ngrams = my_model.create_ngrams_for_word(word,ngram_keys)
@@ -85,6 +88,7 @@ def predict_with_zeroed_ngrams(my_model,word,ngram_keys,keys2,new_model):
             ngram_vector[0,i,:] = 0
     return new_model.predict(ngram_vector)
 
+# compare our results to given dataset
 def compare_to_dataset(dataset_file,ngram_keys,simple_model=None,import_ngrams=None):
     ngram_results = []
     file_data = []
@@ -115,19 +119,18 @@ my_model = helpers.get_model_by_name(nn_type,model=model)
 if import_ngrams is not None:
     simple_model = create_simple_conv_model(my_model)
 
+# run compare alg.
+model.load_weights('weights_'+str(weights)+'.h5')
+weights = None
+for layer in model.layers:
+    if layer.get_config()["name"]=="input_layer":
+        weights = layer.get_weights()[0]
+        break
 
-for i in range(start, num_input_files):
-    model.load_weights('weights0_fo'+str(i)+'_fn'+str(i)+'_copy.h5')
-    weights = None
-    for layer in model.layers:
-        if layer.get_config()["name"]=="input_layer":
-            weights = layer.get_weights()[0]
-            break
-
-    for ds in datasets:
-        if isinstance(my_model, helpers.models.WordVectorModel):
-            print i,ds,compare_to_dataset(datasets[ds],word_keys)
-        else:
-            print i,ds,compare_to_dataset(datasets[ds],ngram_keys,simple_model,import_ngrams)
+for ds in datasets:
+    if isinstance(my_model, helpers.models.WordVectorModel):
+        print i,ds,compare_to_dataset(datasets[ds],word_keys)
+    else:
+        print i,ds,compare_to_dataset(datasets[ds],ngram_keys,simple_model,import_ngrams)
 
 
